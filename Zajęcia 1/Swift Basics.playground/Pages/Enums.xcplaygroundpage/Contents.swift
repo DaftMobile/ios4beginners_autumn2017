b6945bc2b04aa5b85a80fe5e037c4804ce99c54c
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2017-10-22 15:37:25 +0200

    Add playground for class 1

---
+++ Zajęcia 1/Swift Basics.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+import Foundation
+//: [Previous](@previous)
+
+//: # Enums
+
+//: [Next](@next)
---
+++ Zajęcia 1/Swift Basics.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+/*:
+# Podstawy iOS MiMUW
+
+## Spis treści
+
+1.	[Stałe i zmienne](Constants%20and%20Variables)
+2.	[Stringi](Strings)
+3.	[Kolekcje](Collections)
+4.	[Optionale](Optionals)
+5.	[Control Flow](Control%20Flow)
+6.	[Enums](Enums)
+7.	[Funkcje](Functions)
+8.	[Klasy](Classes)
+*/
