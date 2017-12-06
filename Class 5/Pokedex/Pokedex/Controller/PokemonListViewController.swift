ba38bba6ee73715d1c3c5ecee662b243734671fc
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2017-12-06 14:25:03 +0100

    Add Pokemon list solution

--- Class 5/Pokedex/Pokedex/Controller/PokemonListViewController.swift
+++ Class 5/Pokedex/Pokedex/Controller/PokemonListViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class PokemonListViewController: UIViewController {
-	//TODO:
-	// Add TableView as Outlet
-	// Add Constraints
-	// Use the Model
-	// Set self as TableView's delegate and datasource
-	// Implement required methods
+class PokemonListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+	var model: [Pokemon]!
+
+	@IBOutlet weak var tableView: UITableView!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.model = try! PokemonDiskLoader().load()
+	}
+
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return model.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: "CELL") else { fatalError() }
+		let pokemon = model[indexPath.row]
+		cell.textLabel?.text = "#\(pokemon.number): \(pokemon.name)"
+		cell.contentView.backgroundColor = pokemon.keyColor
+		cell.textLabel?.textColor = UIColor.white
+		cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 21.0)
+		return cell
+	}
 }
